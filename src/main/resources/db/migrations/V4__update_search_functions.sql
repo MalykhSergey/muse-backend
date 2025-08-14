5ea08232cc30554803f441e506224acca65b3f27
Malykh Sergey <sergeymalykhdm@gmail.com>
2025-08-14 11:09:03 +0600

    fix: Вернуть get_post_rich.

--- src/main/resources/db/migrations/V4__update_search_functions.sql
+++ src/main/resources/db/migrations/V4__update_search_functions.sql
@@ -41,6 +41,55 @@ p.id, p.title, p.body, p.post_type, p.author_id,
 u.external_id, u.internal_id, u.user_type, u.name,
 p.parent_id, p.answer_id, p.created, p.updated, p.search_vector;
 
+CREATE OR REPLACE FUNCTION get_post_rich(
+    user_id_param BIGINT,
+    post_id_param BIGINT
+)
+RETURNS TABLE (
+    id BIGINT,
+    title VARCHAR,
+    body TEXT,
+    post_type post_type,
+    author_id BIGINT,
+    external_id BIGINT,
+    internal_id UUID,
+    user_type user_type,
+    author_name VARCHAR,
+    parent_id BIGINT,
+    answer_id BIGINT,
+    created TIMESTAMP,
+    updated TIMESTAMP,
+    score BIGINT,
+    answer_count BIGINT,
+    vote_type vote_type,
+    tags JSONB
+) AS $$
+BEGIN
+    RETURN QUERY
+    SELECT
+            pbv.id,
+            pbv.title,
+            pbv.body,
+            pbv.post_type,
+            pbv.author_id,
+            pbv.external_id,
+            pbv.internal_id,
+            pbv.user_type,
+            pbv.name,
+            pbv.parent_id,
+            pbv.answer_id,
+            pbv.created,
+            pbv.updated,
+            pbv.score,
+            pbv.answer_count,
+            v.type,
+            pbv.tags
+    FROM post_base_view pbv
+    LEFT JOIN votes v ON pbv.id = v.post_id AND user_id_param = v.author_id
+    WHERE pbv.id = post_id_param;
+END;
+$$ LANGUAGE plpgsql;
+
 CREATE OR REPLACE FUNCTION get_posts_rich(
     user_id_param BIGINT,
     parent_id_param BIGINT,
