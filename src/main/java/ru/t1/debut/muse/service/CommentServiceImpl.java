5d11ff07d7ab94250c80dc7f0635749841907bc0
Malykh Sergey <sergeymalykhdm@gmail.com>
2025-08-18 15:26:10 +0600

    feat: Отправлять уведомления при создании комментария.

--- src/main/java/ru/t1/debut/muse/service/CommentServiceImpl.java
+++ src/main/java/ru/t1/debut/muse/service/CommentServiceImpl.java
@@ -8,22 +8,35 @@ import ru.t1.debut.muse.dto.CommentDTO;
 import ru.t1.debut.muse.dto.CreateCommentRequest;
 import ru.t1.debut.muse.dto.UpdateCommentRequest;
 import ru.t1.debut.muse.dto.UserDTO;
-import ru.t1.debut.muse.entity.Comment;
-import ru.t1.debut.muse.entity.Post;
-import ru.t1.debut.muse.entity.User;
+import ru.t1.debut.muse.entity.*;
+import ru.t1.debut.muse.exception.ResourceNotFoundException;
 import ru.t1.debut.muse.repository.CommentRepository;
+import ru.t1.debut.muse.repository.PostRepository;
+import ru.t1.debut.muse.repository.PostSubscribeRepository;
 
 import java.time.LocalDateTime;
+import java.util.List;
+import java.util.UUID;
 
 @Service
 public class CommentServiceImpl implements CommentService {
     private final CommentRepository commentRepository;
     private final UserService userService;
+    private final NotificationService notificationService;
+
+    private final PostSubscribeRepository postSubscribeRepository;
+
+    private final PostRepository postRepository;
 
     @Autowired
-    public CommentServiceImpl(CommentRepository commentRepository, UserService userService) {
+    public CommentServiceImpl(CommentRepository commentRepository, UserService userService, NotificationService notificationService,
+                              PostSubscribeRepository postSubscribeRepository,
+                              PostRepository postRepository) {
         this.commentRepository = commentRepository;
         this.userService = userService;
+        this.notificationService = notificationService;
+        this.postSubscribeRepository = postSubscribeRepository;
+        this.postRepository = postRepository;
     }
 
     @Override
@@ -34,11 +47,20 @@ public class CommentServiceImpl implements CommentService {
     @Override
     public CommentDTO create(CreateCommentRequest createCommentRequest, UserDTO authorDTO) {
         User author = userService.getUser(authorDTO);
-        Post post = new Post();
-        post.setId(createCommentRequest.getPostId());
+        Post post = postRepository.findById(createCommentRequest.getPostId()).orElseThrow(ResourceNotFoundException::new);
         LocalDateTime now = LocalDateTime.now();
         Comment comment = commentRepository.save(new Comment(null, createCommentRequest.getBody(), author, post, now, now));
+        sendNotifications(post);
         return new CommentDTO(comment.getId(), comment.getBody(), new UserDTO(author), createCommentRequest.getPostId(), now, now);
+    }
+
+    private void sendNotifications(Post post) {
+        List<UUID> parentPostSubscribers = postSubscribeRepository.findNotificationEnabledUserInternalIdsByPostId(post.getId());
+        parentPostSubscribers.remove(post.getAuthor().getInternalId());
+        EventMessage eventMessage = new EventMessage(EventType.NEW_COMMENT_FOR_POST, parentPostSubscribers);
+        EventMessage eventMessageForPostAuthor = new EventMessage(EventType.NEW_COMMENT_FOR_YOUR_POST, List.of(post.getAuthor().getInternalId()));
+        notificationService.sendNotification(eventMessage);
+        notificationService.sendNotification(eventMessageForPostAuthor);
     }
 
     @Override
