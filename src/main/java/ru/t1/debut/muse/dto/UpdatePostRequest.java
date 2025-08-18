822214c2a0ef0bd8cbc2874516953e5cfd4791bc
Malykh Sergey <sergeymalykhdm@gmail.com>
2025-08-18 12:37:32 +0600

    fix: Исправить валидаторы постов.

--- src/main/java/ru/t1/debut/muse/dto/UpdatePostRequest.java
+++ src/main/java/ru/t1/debut/muse/dto/UpdatePostRequest.java
@@ -1,6 +1,7 @@
 package ru.t1.debut.muse.dto;
 
 import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 import lombok.Data;
 
@@ -10,9 +11,11 @@ import java.util.Set;
 public class UpdatePostRequest {
     @Size(min = 5, max = 150)
     private String title;
+    @NotNull
     @Size(min = 20, max = 5000)
     private String body;
     @Min(1)
     private Long answerId;
+    @NotNull
     private Set<TagDTO> tags;
 }
