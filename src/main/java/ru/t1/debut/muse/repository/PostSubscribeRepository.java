3e1c932819a3964b9f2ae3dc460f2ce47e17920d
Malykh Sergey <sergeymalykhdm@gmail.com>
2025-08-20 21:58:44 +0600

    feat: Оптимизировать запросы в API подписок на посты.

--- src/main/java/ru/t1/debut/muse/repository/PostSubscribeRepository.java
+++ src/main/java/ru/t1/debut/muse/repository/PostSubscribeRepository.java
@@ -4,6 +4,7 @@ import jakarta.transaction.Transactional;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 import ru.t1.debut.muse.entity.PostSubscribe;
@@ -14,9 +15,12 @@ import java.util.UUID;
 
 @Repository
 public interface PostSubscribeRepository extends JpaRepository<PostSubscribe, PostSubscribeId> {
+    @Query("SELECT ps FROM PostSubscribe ps WHERE ps.user.id = :userId")
     Page<PostSubscribe> findAllByUserId(long userId, Pageable pageable);
 
     @Transactional
+    @Modifying
+    @Query("DELETE FROM PostSubscribe ps WHERE ps.post.id = :postId AND ps.user.id = :userId")
     void deleteByPostIdAndUserId(long postId, long userId);
 
     @Query("SELECT ps.user.internalId FROM PostSubscribe ps WHERE ps.postSubscribeId.postId = :postId AND ps.isNotification = true")
